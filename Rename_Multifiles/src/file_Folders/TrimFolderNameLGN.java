f74a00f4d56bdd68d2c64c60d929e82e250c9532
logulgn@live.co.uk <logulgn@live.co.uk>
2015-06-06 11:47:18 +0530

    Added comments

--- Rename_Multifiles/src/file_Folders/TrimFolderNameLGN.java
+++ Rename_Multifiles/src/file_Folders/TrimFolderNameLGN.java
@@ -5,6 +5,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*This class is used to remove the prefix in folder names like "01.new","#000newfolder"
+ * 
+ * @author LGN
+ *
+ *Set your file path in "File folder = new File("**your path**")" 
+ * aslo in last call() need to add your path.
+ */
+
 public class TrimFolderNameLGN {
 	
 	final static File folder = new File("E:\\Songs");	
@@ -24,11 +32,13 @@ public class TrimFolderNameLGN {
 		trimCondtion = null;
 		line = null;
 		fname = null;
-		  
+		  //checking directory 
 		for (File fileEntry : folder.listFiles()) {
 	        if (fileEntry.isDirectory()) {
 	        	line = folder.toString();
 	        	fname = folder.getName();
+	        	
+	        	//prefix checking for main folders
 	        	if(fname.startsWith(".")||
 	     	  			fname.startsWith("_")||
 	     	  			fname.startsWith("#")||
@@ -49,6 +59,8 @@ public class TrimFolderNameLGN {
 	        } else {
 	        	line = folder.toString();
 	        	fname = folder.getName();
+	        	
+	        	//prefix checking for sub folders
 	        	if(fname.startsWith(".")||
 	     	  			fname.startsWith("_")||
 	     	  			fname.startsWith("#")||
@@ -69,69 +81,67 @@ public class TrimFolderNameLGN {
 	    }
 	}
 	
-	
-public static void nameCheckFolder(String fname) throws IOException{
-
-	//check point for filename prefix and remove that _ to 9
-	
-	String letter = null;
-		String folderName = fname;
+	//This method will check the prefix letter and send the letter need to removed
+	public static void nameCheckFolder(String fname) throws IOException{
 		
-		if(folderName.contains("("))
-    		renameFolder(line, folderName, letter="(" );
-		
-		else if(folderName.contains(")"))
-    		renameFolder(line, folderName, letter=")" );
-		
-		else if(folderName.startsWith(" "))
-    		renameFolder(line, folderName, letter=" " );
-    	
-    	else if(folderName.startsWith("."))
-    		renameFolder(line, folderName, letter="." );
-    	
-    	else if(folderName.startsWith("_"))
-    		renameFolder(line, folderName, letter="_" );
-    	
-    	else if(folderName.startsWith("#"))
-    		renameFolder(line, folderName, letter="#" );
-    	
-    	else if(folderName.startsWith("0"))
-    		renameFolder(line, folderName, letter="0" );
-    		
-    	else if(folderName.startsWith("1"))
-    		renameFolder(line, folderName, letter="1" );
-    	
-    	else if(folderName.startsWith("2"))
-    		renameFolder(line, folderName, letter="2");
-    	
-    	else if(folderName.startsWith("3"))
-    		renameFolder(line, folderName, letter="3" );
-    	
-    	else if(folderName.startsWith("4"))
-    		renameFolder(line, folderName, letter="4" );
-    	
-    	else if(folderName.startsWith("5"))
-    		renameFolder(line, folderName, letter="5" );
-    	
-    	else if(folderName.startsWith("6"))
-    		renameFolder(line, folderName, letter="6" );
-    	
-    	else if(folderName.startsWith("7"))
-    		renameFolder(line, folderName, letter="7" );
-    	
-    	else if(folderName.startsWith("8"))
-    		renameFolder(line, folderName, letter="8" );
-    	
-    	else if(folderName.startsWith("9"))
-    		renameFolder(line, folderName, letter="9" );
-    	
-    	else{
-    		return;
-    	}
-		
+		String letter = null;
+			String folderName = fname;
+			
+			if(folderName.contains("("))
+	    		renameFolder(line, folderName, letter="(" );
+			
+			else if(folderName.contains(")"))
+	    		renameFolder(line, folderName, letter=")" );
+			
+			else if(folderName.startsWith(" "))
+	    		renameFolder(line, folderName, letter=" " );
+	    	
+	    	else if(folderName.startsWith("."))
+	    		renameFolder(line, folderName, letter="." );
+	    	
+	    	else if(folderName.startsWith("_"))
+	    		renameFolder(line, folderName, letter="_" );
+	    	
+	    	else if(folderName.startsWith("#"))
+	    		renameFolder(line, folderName, letter="#" );
+	    	
+	    	else if(folderName.startsWith("0"))
+	    		renameFolder(line, folderName, letter="0" );
+	    		
+	    	else if(folderName.startsWith("1"))
+	    		renameFolder(line, folderName, letter="1" );
+	    	
+	    	else if(folderName.startsWith("2"))
+	    		renameFolder(line, folderName, letter="2");
+	    	
+	    	else if(folderName.startsWith("3"))
+	    		renameFolder(line, folderName, letter="3" );
+	    	
+	    	else if(folderName.startsWith("4"))
+	    		renameFolder(line, folderName, letter="4" );
+	    	
+	    	else if(folderName.startsWith("5"))
+	    		renameFolder(line, folderName, letter="5" );
+	    	
+	    	else if(folderName.startsWith("6"))
+	    		renameFolder(line, folderName, letter="6" );
+	    	
+	    	else if(folderName.startsWith("7"))
+	    		renameFolder(line, folderName, letter="7" );
+	    	
+	    	else if(folderName.startsWith("8"))
+	    		renameFolder(line, folderName, letter="8" );
+	    	
+	    	else if(folderName.startsWith("9"))
+	    		renameFolder(line, folderName, letter="9" );
+	    	
+	    	else{
+	    		return;
+	    	}
+			
 	}
 	
-	
+	//This method will create and execute the command in powershell to rename the folder
 	public synchronized static void renameFolder(String line,String folderName,String symb) throws IOException{
 		
 		String newname1 = folderName.replace(symb, "");
@@ -176,7 +186,7 @@ public static void nameCheckFolder(String fname) throws IOException{
     	
 	}
 	
-	
+	//This method to initiate until rename process complete
 	public static void call() throws IOException{
 		
 		File folder = new File("E:\\Songs");
